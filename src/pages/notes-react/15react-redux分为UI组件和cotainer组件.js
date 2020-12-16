e3b1d8fead6baab9c081a5e5b6c4149ca38d7f60
LiangJavaDS <954157304@qq.com>
2020-12-16 19:21:47 +0800

    react-redux的进一步完善

--- src/pages/notes-react/15react-redux分为UI组件和cotainer组件.js
+++ src/pages/notes-react/15react-redux分为UI组件和cotainer组件.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import store from '@/redux/store.js'
+//发现对应文件没有定义组件名，故此组件名可以定义为任意，主要是路径名。
 import App from '@/containers/react-redux/app.jsx'
 import { Provider } from 'react-redux'
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux'
-import { counter } from './reducers'
+import { counter } from '@/redux/reducers.js'
 //生成一个store对象
 const store = createStore(counter)//内部会第一次调用reducer函数
 
