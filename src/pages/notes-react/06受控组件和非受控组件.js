08882ec5f9be7e9ed0bfbd912f94d76e9b3fc350
LiangJavaDS <954157304@qq.com>
2021-01-03 18:48:21 +0800

    事件处理;非受控组件（ref0）和受控组件(onChange）

--- src/pages/notes-react/06受控组件和非受控组件.js
+++ src/pages/notes-react/06受控组件和非受控组件.js
@@ -3,34 +3,34 @@ class userList extends React.Component {
     constructor() {
         super()
         this.state = {
-            pwd:''
+            pwd: ''
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
     }
 
-    handleSubmit(event){
+    handleSubmit(event) {
         let name = this.nameInput.value
         let pwd = this.state.pwd
-        console.log('7878',name,pwd)
-        alert ("用户名为"+name+"密码为"+pwd)
+        console.log('7878', name, pwd)
+        alert("用户名为" + name + "密码为" + pwd)
         event.preventDefault()
     }
 
-    handleChange(event){
+    handleChange(event) {
         let pwd = event.target.value
-        this.setState({pwd})
+        this.setState({ pwd })
     }
 
     render() {
         return (
             <div>
                 <form action='/test' onSubmit={this.handleSubmit}>
-                    {/* 非受控组件，需要手动读取表单输入框的数据 */}
-                    用户名：<input type='text' ref={input => this.nameInput = input}/>
+                    {/* 非受控组件，需要手动读取表单输入框的数据，现用现取 */}
+                    用户名：<input type='text' ref={input => this.nameInput = input} />
                     {/* 受控组件，自己去读取密码框的数据 */}
-                    密码：<input type='password' value={this.state.pwd} onChange={this.handleChange}/>
-                    <input type='submit' value='登录'/>
+                    密码：<input type='password' value={this.state.pwd} onChange={this.handleChange} />
+                    <input type='submit' value='登录' />
                 </form>
             </div >
         )
